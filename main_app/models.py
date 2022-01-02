8300a61c2eb129765696caf9ceb7767029c4bc01
giorgiovh <jorgevonhoroch@gmail.com>
2022-01-02 16:26:04 -0500

    creates Collection model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Collection(models.Model):
+    name = models.CharField(max_length=100)
+    opensea_url = models.CharField(max_length=10000)
+    banner_url = models.CharField(max_length=10000)
+    num_editors = models.IntegerField()
+    num_traits = models.IntegerField()
+    created_date = models.DateField()
+    total_sales = models.IntegerField()
+    num_nfts = models.IntegerField()
+    num_owners = models.IntegerField()
+    average_price = models.DecimalField()
+    seven_day_sales = models.IntegerField()
+    has_discord = models.BooleanField()
+    has_twitter = models.BooleanField()
+    has_instagram = models.BooleanField()
+    has_wiki = models.BooleanField()
+    description_words = models.IntegerField()
+    cluster = models.CharField(max_length=100)     
+
