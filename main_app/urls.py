c28b8228683522e34d36591c6a87f708c1aa40a6
giorgiovh <jorgevonhoroch@gmail.com>
2022-01-02 17:31:01 -0500

    adds collections_detail url path

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('collections/', views.collections_index, name='collections_index')
+    path('collections/', views.collections_index, name='collections_index'),
+    path('collections/<int:collection_id>/', views.collections_detail, name='collections_detail')
 ]
