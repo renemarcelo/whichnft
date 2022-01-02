3b3b514a9cff6dd606055fbd78c3f3a458e4e3f9
giorgiovh <jorgevonhoroch@gmail.com>
2022-01-02 16:38:03 -0500

    overrides __str__ method for Collection model

--- main_app/models.py
+++ main_app/models.py
@@ -20,3 +20,6 @@ class Collection(models.Model):
     description_words = models.IntegerField()
     cluster = models.CharField(max_length=100)     
 
+    def __str__(self):
+        return self.name
+    
